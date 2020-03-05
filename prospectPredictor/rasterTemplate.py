e43067972e7a89f221e039c1269e5925b9c7a156
Tyler Acorn <tacorn@ualberta.ca>
2020-03-05 14:30:45 -0700

    changed the raster template so that it sets some more useful class variables

--- prospectPredictor/rasterTemplate.py
+++ prospectPredictor/rasterTemplate.py
@@ -4,40 +4,39 @@ import numpy as np
 
 
 class RasterTemplate():
-    '''
-    initialize a raster template that will be used by the predictor class for creating the prediction and distance rasters
-    '''
-    def __init__(self, projBounds, cellWidthX, cellHeightY, crs, rasterDTypes='float32', transform='default', driver='GTiff'):
-        self.projBounds:DataFrame = projBounds
+    def __init__(self, projBounds, cellWidthX, cellHeightY, crs, 
+                 rasterDTypes='float32', transform='default', driver='GTiff'):
+        '''
+        initialize a raster template that will be used by the predictor class for creating the prediction and distance rasters
+        '''
+        self.projBounds:pd.DataFrame = projBounds
         self.cellWidthX:np.number = cellWidthX
         self.cellHeightY:np.number = cellHeightY
+        self.minX = int(self.projBounds['minx'][0]) // self.cellWidthX * self.cellWidthX
+        self.maxX = int(self.projBounds['maxx'][0]) // self.cellWidthX * self.cellWidthX
+        self.minY = int(self.projBounds['miny'][0]) // self.cellHeightY * self.cellHeightY
+        self.maxY = int(self.projBounds['maxy'][0]) // self.cellHeightY * self.cellHeightY
+        self.xDim = self.maxX - self.minX
+        self.yDim = self.maxY - self.minY
+        self.nColsX = int(self.xDim / self.cellWidthX)
+        self.nRowsY = int(self.yDim / self.cellHeightY)
         self.crs = crs
         self.dtypes = rasterDTypes
         if isinstance(transform, str):
             if transform == 'default':
-                minX = int(self.projBounds['minx'][0]) // self.cellWidthX * self.cellWidthX
-                maxY = int(self.projBounds['maxy'][0]) // self.cellHeightY * self.cellHeightY
-                self.transform = rasterio.transform.from_origin(minX, maxY, cellWidthX, cellHeightY)
+                self.transform = rasterio.transform.from_origin(self.minX, 
+                                                                self.maxY, 
+                                                                self.cellWidthX, 
+                                                                self.cellHeightY)
             else:
                 raise ValueError("currently only default transforms are supported. you can pass your own transform in a rasterio.transform format")
         else:
             self.transform = transform
-        self.data = None
-        #self._createEmptyRaster()
         self.driver = driver
 
     def initializeEmptyRaster(self):
         '''
         create an empty raster and set to self.data based on paramaters passed)
         '''
-        minX = int(self.projBounds['minx'][0]) // self.cellWidthX * self.cellWidthX
-        maxX = int(self.projBounds['maxx'][0]) // self.cellWidthX * self.cellWidthX
-        minY = int(self.projBounds['miny'][0]) // self.cellHeightY * self.cellHeightY
-        maxY = int(self.projBounds['maxy'][0]) // self.cellHeightY * self.cellHeightY
 
-        xDim = maxX - minX
-        yDim = maxY - minY
-        nColsX = int(xDim / self.cellWidthX)
-        nRowsY = int(yDim / self.cellHeightY)
-
-        return np.full((nRowsY, nColsX), np.nan, dtype=self.dtypes)
+        return np.full((self.nRowsY, self.nColsX), np.nan, dtype=self.dtypes)
